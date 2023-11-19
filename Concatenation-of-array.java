bb204a2717cf50e339b92a7086137b3b962150c1
Muhammad Zohaib <mzohaib.webdoctor@gmail.com>
2023-11-20 00:41:26 +0400

    Concatenation of Array

---
+++ Concatenation-of-array.java
@@ -0,0 +1,25 @@
+public class ConcatenateArray {
+    public static int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[2 * n];
+        
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+        
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        int[] concatenatedArray = getConcatenation(nums);
+
+        System.out.print("Concatenated Array: ");
+        for (int num : concatenatedArray) {
+            System.out.print(num + " ");
+        }
+    }
+}
+
+//git
