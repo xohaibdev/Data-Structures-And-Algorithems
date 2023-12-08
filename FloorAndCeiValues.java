58c5744f323ae9115f687f322a2630c791fc1eb2
Muhammad Zohaib <mzohaib.webdoctor@gmail.com>
2023-12-08 18:20:47 +0400

    floor and ceil value problem description added with test cases

--- FloorAndCeiValues.java
+++ FloorAndCeiValues.java
@@ -1,3 +1,26 @@
+/*
+
+Given an unsorted array arr[] and an element x, find floor and ceiling of x in arr[0..n-1].
+Floor of x is the largest element which is smaller than or equal to x. Floor of x doesn’t exist if x is smaller than smallest element of arr[].
+Ceil of x is the smallest element which is greater than or equal to x. Ceil of x doesn’t exist if x is greater than greatest element of arr[].
+
+Examples: 
+
+Input : arr[] = {5, 6, 8, 9, 6, 5, 5, 6}      
+        x = 7
+Output : Floor = 6
+         Ceiling = 8
+
+Input : arr[] = {5, 6, 8, 9, 6, 5, 5, 6}      
+        x = 6
+Output : Floor = 6
+         Ceiling = 6
+
+Input : arr[] = {5, 6, 8, 9, 6, 5, 5, 6}      
+        x = 10
+Output : Floor = 9
+         Ceiling doesn't exist.
+         */
 class FloorAndCeiValues {
     public static void main(String[] args) {
         int[] testArray = {5,5,5,6,6,8,10};
